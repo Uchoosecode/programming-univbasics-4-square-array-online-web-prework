d79c58cbe3a46cb8ee91fd1efa897c7490e7fdb2
Ayako Wiltshire <awittyl44@gmail.com>
2019-10-25 16:26:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,14 +9,5 @@ end
 squarred
 end
 
-# def square_array(array)
-#   squarred = []
-#   counter = 0 
-#   while array[counter] do
-#   squarred << array[counter] ** 2
-#   counter += 1 
-# end
-# return squarred
-# end
 numbers = [1, 2, 3]
 square_array(numbers)
