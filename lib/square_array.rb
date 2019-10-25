44390aaf918d91e19bfe5e2a61c742bfb04fd664
Ayako Wiltshire <awittyl44@gmail.com>
2019-10-25 16:07:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,9 @@ numbers = [1, 2, 3]
 new_numbers = [9, 10, 16, 25]
 def square_array(array)
   squarred = []
-  i = 0  
-  while i <= array.length do 
-  squarred << array[i] **2
+  counter = 0  
+  while counter <= array.length do 
+  squarred << array[counter] **2
   i += 1
   
 end
