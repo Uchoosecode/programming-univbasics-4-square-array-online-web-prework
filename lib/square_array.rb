1321f26bf105ccd8896e2203ef99dec8412e9bd5
Ayako Wiltshire <awittyl44@gmail.com>
2019-10-25 16:30:30 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+
 def square_array(array)
-  # your code here
+  squarred = []
+  counter = 0 
+  while array[counter] do
+  squarred << array[counter] ** 2
+  counter += 1 
 end
+return squarred
+end
+numbers = [1, 2, 3]
+square_array(numbers)
