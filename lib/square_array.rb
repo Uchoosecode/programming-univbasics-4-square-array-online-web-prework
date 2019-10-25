5a61d271bbbefb49d92cd94b259f03be5a74c3e3
Ayako Wiltshire <awittyl44@gmail.com>
2019-10-25 15:04:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,11 +3,10 @@ new_numbers = [9, 10, 16, 25]
 def square_array(array)
   i = 0 
   while i <= array.length do 
-  p array[i] 
-  
+  i ** 2 
   i += 1
+  puts i
 end
-p array ** 2
 end
 p square_array(numbers)
 p square_array(new_numbers)
